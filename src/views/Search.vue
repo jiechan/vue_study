ef508bf0f4783b16cb6f450141eed0fb6cbfb27a
jie_chen <jie_chen@outlook.com>
2019-01-24 17:55:22 +0800

    增加单选互斥组件以及多组互斥单选组件

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -12,7 +12,22 @@
 
         <div class="loading-layer" v-show="showloading">    
             <lottie :options="defaultOptions" :height="200" :width="200" v-on:animCreated="handleAnimation"/>
-        </div>        
+        </div>   
+
+        <div class="choice-group">
+            <span class="choice-btn" v-for="item in choices" v-bind:class="currentcode === item.code ? 'active-btn': '' "
+            v-bind:key="item.code" @click="choosed(item)">{{item.name}}</span>
+        </div>
+
+        <div style="background: grey; height: 10px"></div>
+
+        <div v-for="spec in specs" v-bind:key="spec.index"> 
+            <p>{{spec.label}}</p>
+            <div class="choice-group">
+                <span class="choice-btn" v-for="detail in spec.details" v-bind:class="currentSpec.includes(detail.name) ? 'active-btn': '' "
+                v-bind:key="detail.code" @click="selectSpec(detail, spec.label)">{{detail.name}}</span>
+            </div>
+        </div>
     </div>
 </template>
 <script>
@@ -23,7 +38,70 @@ export default {
         showloading: false,
         defaultOptions: {animationData: animationData},
         animationSpeed: 1,
+        currentChoiceCode: "",
+        activeSpecs: [],
+        activeSpecStr: "",
+        choices: [
+            {"code":"s01","name":"香蕉"},
+            {"code":"s02","name":"苹果"},
+            {"code":"s03","name":"菠萝"}
+        ],
+        specs: [
+            {
+                "label":"组1",
+                "details":[
+                    {"code":"s00","name":"曲奇"},
+                    {"code":"s01","name":"蛋糕"},
+                    {"code":"s02","name":"橡皮糖"}
+                ]
+            },
+            {
+                "label":"组2",
+                "details":[
+                    {"code":"x10","name":"蘑菇"},
+                    {"code":"x11","name":"番茄"},
+                ]
+            },
+            {
+                "label":"组3",
+                "details":[
+                    {"code":"p20","name":"可乐"},
+                    {"code":"p21","name":"雪碧"},
+                    {"code":"p22","name":"啤酒"},
+                    {"code":"p23","name":"豆奶"},
+                ]
+            }
+        ]
       }
+    },
+    computed: {
+        currentcode() {
+            if(this.currentChoiceCode === "") {
+                return this.choices[0].code;
+            } else {
+                return this.currentChoiceCode;
+            }
+        },
+        currentSpec() {
+            // alert(this.activeSpecs);
+            for(let item of this.activeSpecs){
+                 /* eslint-disable */ 
+                this.activeSpecStr += item.name;
+            }
+            // eslint-disable-next-line
+            console.log(this.activeSpecStr);
+            if(this.activeSpecStr === "") {
+                for(let item of this.specs){
+                    this.activeSpecStr += item.details[0].name;
+                    let o = new Object();
+                    o.label = item.label;
+                    o.code = item.details[0].code;
+                    o.name = item.details[0].name;
+                    this.activeSpecs.push(o);
+                }
+            }
+            return this.activeSpecStr;
+        },
     },
     methods: {
         handleAnimation: function (anim) {
@@ -35,6 +113,27 @@ export default {
         showLoadingLayer: function () {
             this.showloading = true;
         },
+        choosed: function (item) {
+            
+            // console.log(item);
+            this.currentChoiceCode = item.code;
+        },
+        selectSpec(detail, grouplabel) {
+            this.activeSpecStr = '';
+            // console.log(detail);
+            for(var i = this.activeSpecs.length -1; i >= 0 ; i--) {
+                if(this.activeSpecs[i].label == grouplabel) {
+                    this.activeSpecs.splice(i, 1);
+                }
+            }
+            let o = new Object();
+            o.label = grouplabel;
+            o.code = detail.code;
+            o.name = detail.name;
+            this.activeSpecs.push(o);
+            // eslint-disable-next-line
+            console.log(this.activeSpecs);
+        }
     }
 }
 </script>
@@ -96,5 +195,24 @@ export default {
     left: 50%;
     top: 50%;
     transform: translate(-50%, -50%);
+}
+.choice-group {
+    height: 40px;
+    padding: 10px;
+    line-height: 40px;
+}
+.choice-btn {
+    height: 40px;
+    background: #f5f5f5;
+    border: none;
+    border-radius: 6px;
+    display: inline-block;
+    min-width: 80px;
+    margin-right: 20px; 
+    color: #333;
+}
+.active-btn {
+    background: rgba(35, 149, 255, 0.2);
+    color: rgb(35, 149, 255);
 }
 </style>
